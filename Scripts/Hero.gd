13b89ddb94a939907c5aead9bc9089c81e12f84c
winterjorge <64051041+winterjorge@users.noreply.github.com>
2025-11-14 02:41:24 -0500

    heroes can attack

--- Scripts/Hero.gd
+++ Scripts/Hero.gd
@@ -7,17 +7,13 @@ extends CharacterBody2D
 const SPEED = 50.0
 
 func get_closest_enemy():
-	var shortest_distance = INF # Initialize with a very large number
+	var shortest_distance = 99999 # Initialize with a very large number
 
 		# Get all nodes in the "enemies" group
 	var enemies = get_tree().get_nodes_in_group("enemyGroup")
 
 	for enemy in enemies:
-			# Ensure the enemy node is valid and not freed
-		if not is_instance_valid(enemy):
-			continue
 			# Calculate the distance to the current enemy
-			# Use distance_squared_to() for performance if only comparing distances
 		var distance = global_position.distance_to(enemy.global_position)
 			# If this enemy is closer than the current shortest distance, update
 		if distance < shortest_distance:
@@ -27,15 +23,14 @@ func get_closest_enemy():
 
 func _physics_process(_delta: float) -> void:
 	var enemy = get_closest_enemy()
-
+	var enemy_distance = global_position.distance_to(enemy.global_position)
 	var direction = position.direction_to(enemy.global_position)
 	velocity = direction * SPEED
-
-	if direction.length() > 0.2:
-		animated_sprite.play("Walk")
+	
+	if enemy_distance <= 50:
+		animated_sprite.play("Attack_1")
 	else:
-		velocity = Vector2.ZERO
-		animated_sprite.play("Attack")
+		animated_sprite.play("Walk")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
