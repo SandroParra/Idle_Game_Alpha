cc8674036bc05f7630ec8d647ff6c529af70efd9
Sandro Parra Acosta <sandroparra123@gmail.com>
2025-11-13 20:09:08 -0500

    Changes from gravity

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @export var move_speed: float = 55.0
 
-
 @onready var hero = get_tree().get_first_node_in_group("heroGroup")
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -10,10 +9,10 @@ func _physics_process(delta: float) -> void:
 	var direction = position.direction_to(hero.get_node("CollisionShape2D").global_position)
 	velocity = direction * move_speed
 
-	if direction.length() > 0.1:
+	if direction.length() > 0.2:
 		velocity = direction * move_speed
 		animated_sprite.play("Walk")
 	else:
 		velocity = Vector2.ZERO
-		animated_sprite.play("Idle")
+		animated_sprite.play("Attack")
 	move_and_slide()
