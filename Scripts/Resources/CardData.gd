27177b0e94f1f441e031b694c57d29bda2902061
winterjorge <64051041+winterjorge@users.noreply.github.com>
2025-11-27 20:32:47 -0500

    reorganizacion de folders y files

---
+++ Scripts/Resources/CardData.gd
@@ -0,0 +1,9 @@
+class_name CardData extends Resource
+
+@export var name: String = "Heroe"
+@export var cost: int = 3
+@export var icon: Texture2D
+@export var unit_scene: PackedScene # La escena 3D que se instanciará
+@export var health: int = 100
+@export var damage: int = 10
+@export var move_speed: float = 3.0
